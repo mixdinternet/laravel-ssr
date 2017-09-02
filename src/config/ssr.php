e2bd9522297f887aed8defb427a7b9ba489e3078
Irineu <irineu@mixd.com.br>
2017-09-02 19:06:33 +0000

    Add comment on config file

--- src/config/ssr.php
+++ src/config/ssr.php
@@ -1,22 +1,22 @@
 <?php
 return [
 
-    /*
-     */
-
     'enable' => true,
 
     /*
+     * lifetime in minutes of generated pages
      */
 
     'lifetime' => 60*24*7,
 
     /*
+     * request timeout of phantomjs
      */
 
     'timeout' => 5000,
 
     /*
+     * location of your phantomjs binary
      */
 
     'phantom_location' => storage_path('app/phantomjs'),
